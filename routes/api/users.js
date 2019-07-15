57a77a88195162e8356e49ea26c74a1e4adcd5e7
ashwin <ashwin.ashwinkhanna@gmail.com>
2019-07-15 14:44:49 -0400

    added express validator to the routes/api/users.js to validate name email and passwords

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const router = express.Router()
+const express     = require('express')
+const router      = express.Router()
+const { check, validationResult } = require('express-validator/check')
 
 // @route   POST api/users
 // @access  Public (no web token needed)
-router.post('/', (req, res) => {
-  console.log(req.body)
+router.post('/', [
+  check('name', 'name is required').not.isEmpty(),
+  check('email', 'please input a valid email').isEmail(),
+  check('password', 'password must have at least 6 characters').isLength({ min: 6 })
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  
   res.send('User Routes')
 })
 
