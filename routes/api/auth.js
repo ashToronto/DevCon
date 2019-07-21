0eda8686d18f916b582f4b99049f05bcf1d7a639
ashwin <ashwin.ashwinkhanna@gmail.com>
2019-07-21 18:11:36 -0400

    in routes/auth.js replaced the response message with a querry to retrievethe user profile details, excluding password, as a json response which finds the appropriate user based on user.id

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,18 @@
 const express   = require('express')
 const router    = express.Router()
 const auth      = require('../../middleware/auth')
+const users     = require('../../models/User')
 
 // @route   GET api/auth
 // @access  Public (no web token needed)
-router.get('/', auth, (req, res) => res.send('Authentication Routes'))
+// returns profile excluding password
+router.get('/', auth, async(req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch(err) {
+    res.status(500).json({msg: 'server error could not get profile details'})
+  }
+})
 
 module.exports = router
