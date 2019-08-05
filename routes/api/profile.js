425a0de660ce3f442ef2e2d2f4ee19aa8f89a955
ashwin <ashwin.ashwinkhanna@gmail.com>
2019-08-05 12:48:58 -0400

    wrote the get request to fetch github api data which is public for all users to see, returns top 10 respos in asc order sorted in routes/api/profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,8 @@ const auth                              = require('../../middleware/auth')
 const Profile                           = require('../../models/profile')
 const User                              = require('../../models/User')
 const { check, validationResult }       = require('express-validator')
+const request                           = require('request')
+const config                            = require('config')
 
 // @route   GET api/profile will get all user profiles
 // @desc    GET api/profile/me will get my profile
@@ -218,7 +220,33 @@ router.delete('/education/:edu_id', auth, async (req,res) => {
 
 // @route   Get api/profile/github/:username
 // @desc    get github repos for this user
-// @access  Private
+// @access  Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 
 
