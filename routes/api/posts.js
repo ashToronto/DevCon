3bf68914a61a5550f15707aff06ef042515f2019
ashwin <ashwin.ashwinkhanna@gmail.com>
2019-08-10 20:56:51 -0400

    wrote the get request for api/routes/post for retrieving all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,8 +9,9 @@ const { check, validationResult }       = require('express-validator')
 const request                           = require('request')
 const config                            = require('config')
 
-// @route   GET api/posts
-// @access  Public (no web token needed)
+// @route   Post api/posts
+// @desc    Create a user post
+// @access  Private
 router.post('/', [auth,
   [
     check('text','Text is required')
@@ -23,24 +24,36 @@ router.post('/', [auth,
     if (!errors.isEmpty()) {
       return res.status(400).json({})
     }
-    const user = await Post.findById({req.user.id}).populate('user', ['name', 'avatar'])
 
     try {
+      const user = await Post.findById({req.user.id}).populate('user', ['name', 'avatar'])
 
+      const newPost = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.body.id
+      }
+
+      const post = await newPost.save()
+      res.json(post)
     } catch(err){
       console.error(err.message)
       res.status(500).send('Server error')
     }
+  });
 
-    const newPost = {
-      text: req.body.text,
-      name: user.name,
-      avatar: user.avatar,
-      user: req.body.id
+  // @route   Get api/posts
+  // @desc    Get all posts
+  // @access  Private
+  router.get('/', auth, async (req, res) => {
+    try {
+      const posts = await Post.find({}).sort({ date: -1 })
+      res.json(posts)
+    } catch {
+      console.error(err.message)
+      res.status(500).send('Server error')
     }
-
-
-
-  })
+  });
 
 module.exports = router
